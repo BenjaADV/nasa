3c96176af05fcd33a24456d99ff7e1603d43fd4f
BenjaADV <benjamin.alegre@adviters.com>
2025-05-06 17:02:50 -0300

    Added white color in header

--- src/components/layout/dashboard/DashboardHeader.tsx
+++ src/components/layout/dashboard/DashboardHeader.tsx
@@ -10,7 +10,7 @@ import {
 
 export function DashboardHeader() {
   return (
-    <header className="h-16 border-b flex items-center justify-between px-4">
+    <header className="h-16 bg-white border-b border-gray-100 flex items-center justify-between px-4">
       <div className="flex items-center">
         <div className="mr-8"></div>
       </div>
